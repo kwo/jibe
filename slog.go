9a2bd79450259fe1757472e2d3eaf9674d5c874e
Karl Ostendorf <karl@ostendorf.com>
2024-06-27 21:56:23 +0200

    fully implement slog handler

--- slog.go
+++ slog.go
@@ -8,17 +8,37 @@ import (
 // WithSlogHandler returns a slog.Handler that extracts the ID from the context
 // and adds it to the record.
 func WithSlogHandler(idName string, h slog.Handler) slog.Handler {
-	return idHandler{Handler: h, idName: idName}
+	return &idHandler{handler: h, idName: idName}
 }
 
 type idHandler struct {
-	slog.Handler
-	idName string
+	idName  string
+	handler slog.Handler
 }
 
-func (h idHandler) Handle(ctx context.Context, r slog.Record) error {
+func (h *idHandler) Enabled(ctx context.Context, l slog.Level) bool {
+	return h.handler.Enabled(ctx, l)
+}
+
+func (h *idHandler) Handle(ctx context.Context, r slog.Record) error {
 	if id := GetID(ctx); id != "" {
 		r.Add(h.idName, slog.StringValue(id))
 	}
-	return h.Handler.Handle(ctx, r)
+	return h.handler.Handle(ctx, r)
+}
+
+// WithAttrs implements slog.Handler.
+func (h *idHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &idHandler{
+		idName:  h.idName,
+		handler: h.handler.WithAttrs(attrs),
+	}
+}
+
+// WithGroup implements slog.Handler.
+func (h *idHandler) WithGroup(name string) slog.Handler {
+	return &idHandler{
+		idName:  h.idName,
+		handler: h.handler.WithGroup(name),
+	}
 }
